b351a14ff8cf6f73e07c460261a06b4ac1a514df
v4 <89064392+v4lex@users.noreply.github.com>
2023-05-24 17:27:15 +0200

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,9 @@
+new Typed('#typed',{
+    strings : ['valex','social media services','well reputable', 'rich', 'trusted', 'known'],
+    typeSpeed : 40,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+  });
+
+
